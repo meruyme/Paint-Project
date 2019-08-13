27fc4e61c1c7c3b93d4908b4ccc241cd2f06b112
Mellany Linhares <mellinhares6@gmail.com>
2019-08-12 22:10:01 -0300

    Initial commit

---
+++ exceptions/ArquivoVazio.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/**
+ * Exception para verificar se o ArrayList está vazio antes de salvar o arquivo.
+ * @author Mellany Linhares
+ *
+ */
+public class ArquivoVazio extends Exception{
+	
+	/**
+	 * Construtor para o lançamento da exception.
+	 * @param message String - Mensagem de erro que será mostrada ao usuário.
+	 */
+	public ArquivoVazio(String message) {
+		super(message);
+	}
+
+}
