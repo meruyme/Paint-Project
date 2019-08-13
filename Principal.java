2e35931b7bb2d0de5d4c705de27a08865b88722a
Mellany Linhares <mellinhares6@gmail.com>
2019-08-12 22:45:14 -0300

    Initial commit

---
+++ Principal.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ * Classe principal que irá iniciar o programa e abrir a tela Paint.
+ * @author Mellany Linhares
+ *
+ */
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Paint paint = new Paint();
+		paint.setVisible(true);
+		
+	}
+
+}
